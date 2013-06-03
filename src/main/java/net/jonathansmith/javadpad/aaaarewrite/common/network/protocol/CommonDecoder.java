f2f6fb144118fd9803a2b8c4059970980b1c0daf
JonathanCSmith <jon@jonathansmith.me>
2013-06-03 16:02:22 +0100

    More packet work, decoder can be simpler!!!! -> param count function in packet decodeParameter(param number, channelBuffer)

--- src/main/java/net/jonathansmith/javadpad/aaaarewrite/common/network/protocol/CommonDecoder.java
+++ src/main/java/net/jonathansmith/javadpad/aaaarewrite/common/network/protocol/CommonDecoder.java
@@ -16,87 +16,100 @@
  */
 package net.jonathansmith.javadpad.aaaarewrite.common.network.protocol;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.jboss.netty.buffer.ChannelBuffer;
-
-import net.jonathansmith.javadpad.aaaarewrite.common.network.packet.Packet;
-import net.jonathansmith.javadpad.aaaarewrite.common.network.protocol.decoder.DecodeResult;
-import net.jonathansmith.javadpad.aaaarewrite.common.network.protocol.decoder.RollingDecoder;
 
 /**
  *
  * @author jonathansmith
  */
-public class CommonDecoder extends RollingDecoder<CommonDecoder.DecodingState> {
-    
-    public static enum DecodingState {
-        TYPE,
-        PARAM_COUNT,
-        PARAM_SIZE,
-        PARAM_VALUE;
-    }
-    
-    private Packet packet;
+public class CommonDecoder extends StateDrivenDecoder {
     
     private int type;
     private byte[] id;
     private int nParams;
-    private List<String> params;
+    private List<string> params;
     private byte[] param;
-    
+ 
+    // constructors ----------------------------------------------------------
+ 
     public CommonDecoder() {
         super(DecodingState.TYPE);
     }
-    
+ 
     @Override
-    protected DecodeResult<DecodingState> decode(ChannelBuffer buffer, DecodingState state) throws Exception {
-        switch (state) {
-            case TYPE:
-                this.packet = Packet.getPacket(buffer.readInt());
-                return this.continueDecoding(DecodingState.PARAM_COUNT);
-                
-            case PARAM_COUNT:
-                this.nParams = buffer.readByte();
-                
-                if (this.nParams > 0) {
-                    this.params = new ArrayList<String> (this.nParams);
-                    return this.continueDecoding(DecodingState.PARAM_SIZE);
-                }
-                
-                else {
-                    Packet packet = Packet.getPacket(this.type);
-                    return this.finishedDecoding(packet);
-                }
-                
-            case PARAM_SIZE:
-                this.param = new byte[buffer.readByte()];
-                return this.continueDecoding(DecodingState.PARAM_VALUE);
-                
-            case PARAM_VALUE:
-                buffer.readBytes(this.param);
-                this.params.add(new String(this.param));
-                if (this.params.size() >= this.nParams) {
-                    
-                    return this.finishedDecoding();
-                }
-                
-                else {
-                    return this.continueDecoding(DecodingState.PARAM_SIZE);
-                }
-                
-            default:
-                throw new IllegalStateException("Unknown state: " + state);
-        }
-    }
-    
-    @Override
-    public void cleanup() {
+    protected void cleanup() {
+        // cleanup pending resources allocated for decoding
         this.id = null;
         this.nParams = 0;
         this.type = -1;
         this.param = null;
         this.params = null;
+    }
+
+    @Override
+    protected DecodeResult decode(ChannelBuffer buffer, Enum currentState) throws Exception {
+        switch (currentState) {
+            case TYPE:
+                this.type = buffer.readInt();
+                return this.continueDecoding(DecodingState.ID_SIZE);
+ 
+            case ID_SIZE:
+                // Should be protected for 0 or negative sizes.
+                this.id = new byte[buffer.readByte()];
+                return this.continueDecoding(DecodingState.ID);
+ 
+            case ID:
+                buffer.readBytes(this.id);
+                if (this.type == 1) {
+                    // Lets assume type 1 messages only need id.
+                    Message m = new Type1Message(new String(this.id));
+                    return this.finishedDecoding(m);
+                } else {
+                    // Otherwise continue decoding.
+                    return this.continueDecoding(DecodingState.PARAM_COUNT);
+                }
+ 
+            case PARAM_COUNT:
+                this.nParams = buffer.readByte();
+                // If there are parameters continue decoding, otherwise bail.
+                if (this.nParams > 0) {
+                    this.params = new ArrayList<string>(this.nParams);
+                    return this.continueDecoding(DecodingState.PARAM_SIZE);
+                } else {
+                    Message m = new OtherMessage(new String(this.id));
+                    return this.finishedDecoding(m);
+                }
+ 
+            case PARAM_SIZE:
+                this.param = new byte[buffer.readByte()];
+                return this.continueDecoding(DecodingState.PARAM_VALUE);
+ 
+            case PARAM_VALUE:
+                buffer.readBytes(this.param);
+                this.params.add(new String(this.param));
+                if (this.params.size() >= this.nParams) {
+                    // This was the last parameter, exit.
+                    Message m = new OtherMessage(new String(this.id));
+                    m.setParams(this.params);
+                    return this.finishedDecoding(m);
+                } else {
+                    // Continue reading parameters.
+                    return this.continueDecoding(DecodingState.PARAM_SIZE);
+                }
+ 
+            default:
+                throw new IllegalStateException("Unknown state: " + state);
+        }
+    }
+ 
+    // public classes --------------------------------------------------------
+ 
+    public static enum DecodingState {
+        TYPE,
+        ID_SIZE,
+        ID,
+        PARAM_COUNT,
+        PARAM_SIZE,
+        PARAM_VALUE
     }
 }
